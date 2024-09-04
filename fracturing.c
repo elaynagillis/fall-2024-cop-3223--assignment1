82cfa989bb0a919d13577a1c59e8d6b741ac8ab5
elaynagillis <elaynagillis1@gmail.com>
2024-09-04 21:36:18 +0000

    added spaces

--- fracturing.c
+++ fracturing.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
     printf("Enter the x value for the first point. \n")
     double calculateDistance();
-        px1=0;
-        px2=0;
-        py1=0;
-        py2=0;
+        px1 = 0;
+        px2 = 0;
+        py1 = 0;
+        py2 = 0;
         scanf("%d", &px1);
         printf("Point #1 entered: %d", px1);
     return 0; 
