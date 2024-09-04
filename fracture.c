ee9145892fe40ec1633b5eab8fd05fa170e0eb39
elaynagillis <elaynagillis1@gmail.com>
2024-09-04 21:34:53 +0000

    Setup to scan for X-value of the first point

--- fracture.c
+++ fracture.c
@@ -2,8 +2,13 @@
 #include <math.h>
 
 int main(){
+    printf("Enter the x value for the first point. \n")
     double calculateDistance();
-        scanf(%d, &px1);
-        printf("Point #1 entered: %d");
+        px1=0;
+        px2=0;
+        py1=0;
+        py2=0;
+        scanf("%d", &px1);
+        printf("Point #1 entered: %d", px1);
     return 0; 
 }
