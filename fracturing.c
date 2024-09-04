42854a2591bd2547a161291215355c1cc2529416
elaynagillis <elaynagillis1@gmail.com>
2024-09-04 22:00:24 +0000

    Distance- input for points

--- fracturing.c
+++ fracturing.c
@@ -2,13 +2,26 @@
 #include <math.h>
 
 int main() {
-    printf("Enter the x value for the first point. \n")
+
+
+
     double calculateDistance();
-        px1 = 0;
-        px2 = 0;
-        py1 = 0;
-        py2 = 0;
+
+        // define variables for the points
+        int px1 = 0;
+        int px2 = 0;
+        int py1 = 0;
+        int py2 = 0;
+
+        printf("Enter the x value for the first point. \n");
         scanf("%d", &px1);
-        printf("Point #1 entered: %d", px1);
+        printf("Enter the x value for the second point. \n");
+        scanf("%d", &px2);
+        printf("Enter the y value for the first point. \n");
+        scanf("%d", &py1);
+        printf("Enter the y value for the second point. \n");
+        scanf("%d", &py2);
+
+        printf("Point #1 entered: x1=%d; y1=%d\nPoint #2 entered: x2=%d; y2=%d\nThe distance between the two points is\n", px1, py1, px2, py2);
     return 0; 
 }
