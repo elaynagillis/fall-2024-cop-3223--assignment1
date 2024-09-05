36eea129c96cd9581027011098b599fc6a157f3e
elaynagillis <elaynagillis1@gmail.com>
2024-09-05 01:25:35 +0000

    Distance-finished

--- fracturing.c
+++ fracturing.c
@@ -21,9 +21,9 @@ int main() {
         scanf("%d", &py1);
         printf("Enter the y value for the second point. \n");
         scanf("%d", &py2);
-        double change_x = pow(px2 -px1,2); 
-        double change_y = pow(py2- py1,2);
-        double distance = sqrt(change_x + change_y);
+        double x_squared = pow((px2 - px1),2);
+        double y_squared = pow((py2 - py1),2);
+        double distance = sqrt(x_squared + y_squared);
 
         printf("Point #1 entered: x1=%d; y1=%d\nPoint #2 entered: x2=%d; y2=%d\nThe distance between the two points is %.2f\n", px1, py1, px2, py2, distance);
     return 0; 
