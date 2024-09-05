0a52c6b127779d5c95ccdcad22e4ad7c374fe152
elaynagillis <elaynagillis1@gmail.com>
2024-09-05 03:42:43 +0000

    Perimeter-added

--- fracturing.c
+++ fracturing.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-double calculatePerimeter() {
-
-
-
-    double calculateDistance() {
-
+int main(void) 
+{
+    double calculateDistance();
         // define variables for the points
         int px1 = 0;
         int px2 = 0;
@@ -24,13 +21,11 @@ double calculatePerimeter() {
         double x_squared = pow((px2 - px1),2);
         double y_squared = pow((py2 - py1),2);
         double distance = sqrt(x_squared + y_squared);
-
         printf("Point #1 entered: x1=%d; y1=%d\nPoint #2 entered: x2=%d; y2=%d\nThe distance between the two points is %.2f\n", px1, py1, px2, py2, distance);
-        return distance;
-    }
-    double pi = 3.14159265;
-    double perimeter = (2 * pi* (distance /2));
-    printf("The perimeter of the city encompassed by your request is %.2f.\n", perimeter);
-    
+        
+    double calculatePerimeter();
+        double PI = 3.14159;
+        double perimeter = (PI * 2 * distance);
+        printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
     return 0; 
 }
