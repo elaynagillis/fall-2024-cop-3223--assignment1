c420c888f2aec0a459350e12dd3027b0f1352e6b
elaynagillis <elaynagillis1@gmail.com>
2024-09-07 01:46:07 +0000

    Printed Points

--- fracturing.c
+++ fracturing.c
@@ -21,6 +21,8 @@
         scanf("%d", &py1);
         printf("Enter the y value for the second point. \n");
         scanf("%d", &py2);
+        printf("Point #1 entered: x1=%d ; y1=%d\nPoint #2 entered: x2=%d ; y2=%d\n", px1, py1, px2, py2);
+        
 
         double x_squared = pow((px2 - px1),2);
         double y_squared = pow((py2 - py1),2);
